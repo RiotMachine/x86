c1014cc0178ff78d7f2dad189ee25ca5184746c8
Jacob J <jirak87@outlook.com>
2026-01-30 16:20:06 -0600

    Working on assembly syntax. Added comments.

--- hello.asm
+++ hello.asm
@@ -5,10 +5,18 @@ JMP start
 msg:    db "Hello, world!", 0Dh,0Ah, 24h
 
 start:  MOV    dx, msg
-        MOV    ah, 09h
-        int    21h
+        MOV    ah, 0x9
+        INT    0x21
 
         MOV    ah, 0
-        int    16h
+        INT    0x16
 
 RET
+
+
+; INT = interrupt, not 'int'
+; h == 0x
+; ';' is comment in Intel syntax
+; "MOV" is a mnemonic representing an opcode
+; syntax is INSTRUCTION destination, source
+; INT is provided a digit representing some particular interrupt
